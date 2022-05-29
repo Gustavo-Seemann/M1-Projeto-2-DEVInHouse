2f8aa69fac7ce3c72a9dc8054962b5a275ff0e7f
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-05-29 16:23:37 -0300

    adiciona vue-gravatar
    instalei o vue3-gravatar para ser usado na nav-bar para gerar o avatar do usuario

--- m1-projeto-2/src/main.js
+++ m1-projeto-2/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.css"
+import VueGravatar from "vue3-gravatar"
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueGravatar).mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
