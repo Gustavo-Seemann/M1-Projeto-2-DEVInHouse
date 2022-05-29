fe7852bf811f6c1e1478a1a3ce18f92171077573
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-05-29 16:53:05 -0300

    adiciona axios e cria rota
    criei rota e adicionei o axios para fazer a consulta da viaCEP API.

--- m1-projeto-2/src/router/index.js
+++ m1-projeto-2/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/viewsAuth/Login.vue'
 import Register from '../views/viewsAuth/Register.vue'
 import Cadastroitems from '../views/dashboard/Cadastroitems.vue'
+import CadastroColab from '../views/dashboard/CadastroColab.vue'
+
 
 
 const routes = [
@@ -19,6 +21,11 @@ const routes = [
     path: '/cadastroitem',
     name: 'CADASTRO DE ITENS',
     component: Cadastroitems
+  },
+  {
+    path: '/cadastrocolab',
+    name: 'CADASTRO DE COLABORADORES',
+    component: CadastroColab
   },
 ]
 
