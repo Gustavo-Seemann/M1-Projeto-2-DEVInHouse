fb61b00de71e605e62a3459b28c30b36b0a89442
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-05-29 17:22:18 -0300

    adiciona store para pesquisaitem
    adicionei um store para as mutations e actions de filtrar a procurar de itens.

--- m1-projeto-2/src/store/index.js
+++ m1-projeto-2/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import autenticacaoModule from './modules/autenticacaoModule'
 import pesquisaColabModule from './modules/pesquisaColabModule'
+import pesquisaItemModule from './modules/pesquisaItemModule'
 
 
 
@@ -8,5 +9,7 @@ export default createStore({
   modules: {
     autenticacaoModule,
     pesquisaColabModule,
+    pesquisaItemModule,
+
   }
 })
