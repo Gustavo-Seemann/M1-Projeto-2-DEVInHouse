0b744cd2ed0297f6109654933fddfadfcf98fd6e
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-05-29 15:01:46 -0300

    adiciona textinput
    Adicionei um componente de TextInput que irei utilizar no registro e no login do projeto

--- m1-projeto-2/src/views/viewsAuth/Login.vue
+++ m1-projeto-2/src/views/viewsAuth/Login.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <h1> Pagina de login <h2>
+    <h1> Pagina de login </h1>
 </div>
 </template>
 
---
+++ m1-projeto-2/src/views/viewsAuth/Register.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> Pagina de Registro <h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
